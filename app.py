617adedcf84981b3b78229280eb4b52301b01c8c
Yashmaurya7 <mauryayash2011@gmail.com>
2024-12-04 08:06:04 +0530

    newPR

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title("Add Two Numbers")
 
+
+
 # Inputs for the two numbers
 num1 = st.number_input("Enter the first number:", step=1.0)
 num2 = st.number_input("Enter the second number:", step=1.0)
